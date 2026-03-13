f232d310b5e03245e180262b9b543ab39ba2cd8a
Nawab Aarij Imam <nawabaarij@gmail.com>
2026-03-13 21:24:18 +0100

    chore: cleanup unused params

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,15 @@ import path from 'path'
 import { fileURLToPath } from 'url';
 import { transcribeAudio } from "./pipeline/stt.js";
 
-function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
+// This is a sample code to test the functionality of the STT pipeline using nodejs-whisper library.
 
-
+// Get the current directory of the file
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const __sampleAudioFilePath = path.resolve(__dirname, './audio/test_audio.wav');
-// Need to provide exact path to your audio file.
 const filePath = path.resolve(__dirname, '../audio/test_audio.wav')
 
-
+// Transcribe the audio file and log the transcribed text and the time taken for the STT process.
 let transcibedText: string = await transcribeAudio(filePath);
 
 console.log("Transcribed Text:", transcibedText);
--- src/pipeline/stt.ts
+++ src/pipeline/stt.ts
@@ -10,20 +10,12 @@ export async function transcribeAudio(filePath: string): Promise<string> {
         autoDownloadModelName: 'small.en', // (optional) auto download a model if model is not present
         removeWavFileAfterTranscription: false, // (optional) remove wav file once transcribed
         withCuda: false, // (optional) use cuda for faster processing
-        logger: console, // (optional) Logging instance, defaults to console
         whisperOptions: {
             outputInCsv: false, // get output result in csv file
-            outputInJson: false, // get output result in json file
-            outputInJsonFull: false, // get output result in json file including more information
             outputInLrc: false, // get output result in lrc file
             outputInSrt: false, // get output result in srt file
             outputInText: true, // get output result in txt file
             outputInVtt: false, // get output result in vtt file
-            outputInWords: false, // get output result in wts file for karaoke
-            translateToEnglish: false, // translate from source language to english
-            wordTimestamps: false, // word-level timestamps
-            timestamps_length: 20, // amount of dialogue per timestamp pair
-            splitOnWord: true, // split on word rather than on token
         },
     })
     
